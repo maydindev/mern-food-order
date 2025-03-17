0f12f06c31f2f3064a779ea5f6ac9c6dc777f63d
Mehmet AYDIN <153236487+maydindev@users.noreply.github.com>
2025-03-17 11:05:22 +0300

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <div className="bg-secondary text-white">
+    <div className="bg-secondary text-white min-h-screen">
       <div className="container mx-auto pt-16 pb-6">
         <div className="flex md:justify-between justify-center text-center flex-wrap md:gap-y-0 gap-y-6 ">
           <div className="md:flex-1">
