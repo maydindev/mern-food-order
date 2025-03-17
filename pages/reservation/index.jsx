f3629e2946ef66136bf0dc58d17d46d0fc20fc8c
Mehmet AYDIN <153236487+maydindev@users.noreply.github.com>
2025-03-17 11:09:29 +0300

    Update index.jsx

--- pages/reservation/index.jsx
+++ pages/reservation/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Reservation from "../../components/Reservation";
 
+/*
+<React.Fragment>
+      <Reservation />
+    </React.Fragment>
+*/
+
 const Index = () => {
   return (
-    <React.Fragment>
+    <div className="min-h-screen">
       <Reservation />
     </React.Fragment>
   );
