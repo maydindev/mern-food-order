472d966cd3a7d8970bd5f65fc98f0760be3d0a10
Mehmet AYDIN <153236487+maydindev@users.noreply.github.com>
2025-03-18 10:07:44 +0300

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
           </div>
         </div>
         <p className="text-center mt-10">
-          © 2022 All Rights Reserved By Free Html Templates
+          © 2022 All Rights Reserved
         </p>
       </div>
     </div>
